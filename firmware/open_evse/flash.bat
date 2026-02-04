5de03f246d67a5119c3bfccc30cbe8ed821e0b1e
lincomatic <lincomatic@hotmail.com>
2026-02-03 17:37:09 -0800

    update flash.bat

--- firmware/open_evse/flash.bat
+++ firmware/open_evse/flash.bat
@@ -1,3 +1,2 @@
-avrdude -c USBasp -p m328p -U lfuse:w:0xFF:m -U hfuse:w:0xDF:m -U efuse:w:0x05:m
-avrdude -c USBasp -p m328p -U flash:w:open_evse-384.hex
-pause
+rem avrdude -c USBasp -p m328p -B16 -U lfuse:w:0xFF:m -U hfuse:w:0xDF:m -U efuse:w:0xFD:m
+avrdude -c USBasp -p m328p -B0.5 -U flash:w:%1
